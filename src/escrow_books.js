08bbc8b46e49178a133f1627474582e1394e775b
lathan britz <lathan.britz@drupalconnect.com>
2022-07-20 20:45:21 -0400

    try this

--- src/escrow_books.js
+++ src/escrow_books.js
@@ -138,7 +138,7 @@ module.exports = class escrow_books extends EventEmitter {
                         }
                         if (PubSubManager != null) {
                             // dont send a message if no one is listening
-                            if (Pubsub.checkChannel(json.message.account)) {
+                            if (PubSubManager.checkChannel(json.message.account)) {
                                 log('pushed ', {RATE_UPDATE: rate_update})
                                 PubSubManager.route({RATE_UPDATE: rate_update}, element.account)
                             }
